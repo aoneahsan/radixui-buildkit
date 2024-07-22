a1a9eb60a3b65d4133628979466ff4478059052e
Aoneahsan <aoneahsan@gmail.com>
2024-07-22 07:28:15 +0500

    main -commit

--- src/components/general/Avatar/index.tsx
+++ src/components/general/Avatar/index.tsx
@@ -11,12 +11,19 @@ import { isZNonEmptyString } from "zaions-tool-kit";
 // #endregion
 
 // #region ---- Custom Imports ----
-import { ZAvatar, ZFlex, ZText } from "@src/components/radixUI";
+import {
+  ZAvatar,
+  ZButton,
+  ZFlex,
+  ZPopover,
+  ZText,
+} from "@src/components/radixUI";
 
 // #endregion
 
 // #region ---- Types Imports ----
 import { ZRUAlignE, ZRUAvatarVariantE, ZRUColorE } from "@src/types";
+import ZRUBox from "@src/components/radixUI/ZRUBox";
 
 // #endregion
 
@@ -35,6 +42,7 @@ interface ZUserAvatarI {
   text?: string;
   arrowDownClassName?: string;
   onClick?: () => void;
+  logoutBtnClickHandler?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 const ZUserAvatar: React.FC<ZUserAvatarI> = ({
@@ -48,30 +56,50 @@ const ZUserAvatar: React.FC<ZUserAvatarI> = ({
   text,
   arrowDownClassName,
   onClick,
+  logoutBtnClickHandler,
 }) => {
   return (
-    <ZFlex
-      gap="2"
-      className={ZClassNames(className, "gap-3 cursor-pointer")}
-      align={ZRUAlignE.center}
-      onClick={onClick}
+    <ZPopover
+      trigger={{
+        children: (
+          <ZFlex
+            gap="2"
+            className={ZClassNames(className, "gap-3 cursor-pointer")}
+            align={ZRUAlignE.center}
+            onClick={onClick}
+          >
+            <ZAvatar
+              className={ZClassNames(avatarClassName)}
+              variant={variant}
+              color={color}
+              src={src}
+              fallback={fallback}
+            />
+            {isZNonEmptyString(text) ? (
+              <ZFlex
+                align={ZRUAlignE.center}
+                className="gap-2 hover:text-gray8"
+              >
+                <ZText className={ZClassNames(textClassName)}>{text}</ZText>
+                <ChevronDownIcon
+                  className={ZClassNames(arrowDownClassName, "w-3 h-3")}
+                />
+              </ZFlex>
+            ) : null}
+          </ZFlex>
+        ),
+      }}
     >
-      <ZAvatar
-        className={ZClassNames(avatarClassName)}
-        variant={variant}
-        color={color}
-        src={src}
-        fallback={fallback}
-      />
-      {isZNonEmptyString(text) ? (
-        <ZFlex align={ZRUAlignE.center} className="gap-2 hover:text-gray8">
-          <ZText className={ZClassNames(textClassName)}>{text}</ZText>
-          <ChevronDownIcon
-            className={ZClassNames(arrowDownClassName, "w-3 h-3")}
-          />
-        </ZFlex>
-      ) : null}
-    </ZFlex>
+      <ZRUBox className="space-y-2">
+        <ZButton
+          onClick={logoutBtnClickHandler}
+          className="w-full"
+          color={ZRUColorE.purple}
+        >
+          Logout
+        </ZButton>
+      </ZRUBox>
+    </ZPopover>
   );
 };
 

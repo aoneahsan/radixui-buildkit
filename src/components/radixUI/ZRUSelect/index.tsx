53f448ed1b1b5f834a180ffefda9d2df1b70b36b
Ahsan Mahmood <aoneahsan@gmail.com>
2024-12-06 18:14:31 +0500

    updated version deployed, mainly trying to move package specific exports to nested exports to make it easy for every body to install our packages

--- src/components/radixUI/ZRUSelect/index.tsx
+++ src/components/radixUI/ZRUSelect/index.tsx
@@ -1,196 +1,196 @@
 // #region ---- Core Imports ----
-import React, { useMemo } from "react";
+import React, { useMemo } from 'react';
 
 // #endregion
 
 // #region ---- Packages Imports ----
-import { Select } from "@radix-ui/themes";
-import { ZClassNames } from "zaions-react-tool-kit";
-import { isZNonEmptyString } from "zaions-tool-kit";
+import { Select } from '@radix-ui/themes';
+import { ZClassNames } from 'zaions-react-tool-kit';
+import { isZNonEmptyString } from 'zaions-tool-kit';
 
 // #endregion
 
 // #region ---- Custom Imports ----
-import { ZBox, ZButton, ZFlex, ZText } from "..";
+import { ZBox, ZButton, ZFlex, ZText } from '..';
 
 // #endregion
 
 // #region ---- Types Imports ----
-import { Responsive } from "@radix-ui/themes/props";
+import { Responsive } from '@radix-ui/themes/props';
 import {
-  ZRUAlignE,
-  ZRUColorE,
-  ZRUJustifyE,
-  ZRUOrientationE,
-  ZRUSelectContentPositionE,
-  ZRUTextAsE,
-  type ZRUBasicVariantE,
-  type ZRURadiusE,
-} from "@src/enums/radixUI";
+	ZRUAlignE,
+	ZRUColorE,
+	ZRUJustifyE,
+	ZRUOrientationE,
+	ZRUSelectContentPositionE,
+	ZRUTextAsE,
+	type ZRUBasicVariantE,
+	type ZRURadiusE,
+} from '@src/enums/radixUI';
 import type {
-  ZRUButtonI,
-  ZRUMarginI,
-  ZRUSelectValueI,
-  ZRUShowable,
-} from "@src/types";
+	ZRUButtonI,
+	ZRUMarginI,
+	ZRUSelectValueI,
+	ZRUShowable,
+} from '@src/types';
 
 interface ZRUSelectI extends ZRUShowable {
-  children?: React.ReactNode;
-  label?: string;
-  asChild?: boolean;
-  className?: string;
-  labelClassName?: string;
-  style?: Record<string, unknown>;
-  size?: Responsive<"1" | "2" | "3">;
-  defaultOpen?: boolean;
-  defaultValue?: string;
-  autoComplete?: string;
-  disabled?: boolean;
-  name?: string;
-  open?: boolean;
-  required?: boolean;
-  value?: string;
-  isTouched?: boolean;
-  errorMessage?: string;
-  infoText?: string;
-  triggerClassName?: string;
-  onOpenChange?(open: boolean): void;
-  onValueChange?(value: string): void;
+	children?: React.ReactNode;
+	label?: string;
+	asChild?: boolean;
+	className?: string;
+	labelClassName?: string;
+	style?: Record<string, unknown>;
+	size?: Responsive<'1' | '2' | '3'>;
+	defaultOpen?: boolean;
+	defaultValue?: string;
+	autoComplete?: string;
+	disabled?: boolean;
+	name?: string;
+	open?: boolean;
+	required?: boolean;
+	value?: string;
+	isTouched?: boolean;
+	errorMessage?: string;
+	infoText?: string;
+	triggerClassName?: string;
+	onOpenChange?(open: boolean): void;
+	onValueChange?(value: string): void;
 
-  trigger?: {
-    variant?: ZRUBasicVariantE;
-    color?: ZRUColorE;
-    radius?: ZRURadiusE;
-    placeholder?: string;
-    className?: string;
-  } & ZRUMarginI;
+	trigger?: {
+		variant?: ZRUBasicVariantE;
+		color?: ZRUColorE;
+		radius?: ZRURadiusE;
+		placeholder?: string;
+		className?: string;
+	} & ZRUMarginI;
 
-  content?: {
-    color?: ZRUColorE;
-    highContrast?: boolean;
-    position?: ZRUSelectContentPositionE;
-  };
+	content?: {
+		color?: ZRUColorE;
+		highContrast?: boolean;
+		position?: ZRUSelectContentPositionE;
+	};
 
-  options?: Array<ZRUSelectValueI>;
-  labelOrientation?: ZRUOrientationE;
-  position?: ZRUSelectContentPositionE;
+	options?: Array<ZRUSelectValueI>;
+	labelOrientation?: ZRUOrientationE;
+	position?: ZRUSelectContentPositionE;
 
-  showLabelBtn?: boolean;
-  labelBtnProps?: ZRUButtonI;
+	showLabelBtn?: boolean;
+	labelBtnProps?: ZRUButtonI;
 }
 // #endregion
 
 const ZRUSelect: React.FC<ZRUSelectI> = (props) => {
-  const _isError = useMemo(
-    () =>
-      Array.isArray(props?.errorMessage)
-        ? props?.errorMessage?.length > 0
-        : typeof props?.errorMessage === "string"
-        ? isZNonEmptyString(props?.errorMessage)
-        : false,
-    [props?.errorMessage]
-  );
+	const _isError = useMemo(
+		() =>
+			Array.isArray(props?.errorMessage)
+				? props?.errorMessage?.length > 0
+				: typeof props?.errorMessage === 'string'
+				? isZNonEmptyString(props?.errorMessage)
+				: false,
+		[props?.errorMessage]
+	);
 
-  return (
-    <ZBox
-      show={props?.show}
-      className={ZClassNames(props?.className, {
-        "flex items-center gap-1":
-          props?.labelOrientation === ZRUOrientationE.horizontal,
-      })}
-    >
-      <ZFlex align={ZRUAlignE.center} justify={ZRUJustifyE.between}>
-        {isZNonEmptyString(props?.label) ? (
-          <ZText
-            as={ZRUTextAsE.label}
-            size="1"
-            className={ZClassNames("text-base block", props?.labelClassName)}
-          >
-            {props?.label}
-            {props?.required ? (
-              <ZText
-                as={ZRUTextAsE.span}
-                className="ms-1"
-                color={ZRUColorE.tomato}
-              >
-                *
-              </ZText>
-            ) : null}
-          </ZText>
-        ) : null}
+	return (
+		<ZBox
+			show={props?.show}
+			className={ZClassNames(props?.className, {
+				'flex items-center gap-1':
+					props?.labelOrientation === ZRUOrientationE.horizontal,
+			})}
+		>
+			<ZFlex align={ZRUAlignE.center} justify={ZRUJustifyE.between}>
+				{isZNonEmptyString(props?.label) ? (
+					<ZText
+						as={ZRUTextAsE.label}
+						size='1'
+						className={ZClassNames('text-base block', props?.labelClassName)}
+					>
+						{props?.label}
+						{props?.required ? (
+							<ZText
+								as={ZRUTextAsE.span}
+								className='ms-1'
+								color={ZRUColorE.tomato}
+							>
+								*
+							</ZText>
+						) : null}
+					</ZText>
+				) : null}
 
-        {props?.showLabelBtn ? (
-          <ZButton
-            {...props?.labelBtnProps}
-            size={props?.labelBtnProps?.size ?? "1"}
-            className={ZClassNames(
-              "mb-1 me-2",
-              props?.labelBtnProps?.className
-            )}
-          >
-            {props?.labelBtnProps?.children}
-          </ZButton>
-        ) : null}
-      </ZFlex>
+				{props?.showLabelBtn ? (
+					<ZButton
+						{...props?.labelBtnProps}
+						size={props?.labelBtnProps?.size ?? '1'}
+						className={ZClassNames(
+							'mb-1 me-2',
+							props?.labelBtnProps?.className
+						)}
+					>
+						{props?.labelBtnProps?.children}
+					</ZButton>
+				) : null}
+			</ZFlex>
 
-      <Select.Root
-        size={props.size ?? "3"}
-        name={props.name}
-        open={props.open}
-        value={props.value}
-        disabled={props.disabled}
-        defaultOpen={props.defaultOpen}
-        defaultValue={props.defaultValue}
-        autoComplete={props.autoComplete}
-        onOpenChange={props.onOpenChange}
-        onValueChange={props.onValueChange}
-      >
-        <Select.Trigger
-          {...props?.trigger}
-          className={props.triggerClassName}
-        />
+			<Select.Root
+				size={props.size ?? '3'}
+				name={props.name}
+				open={props.open}
+				value={props.value}
+				disabled={props.disabled}
+				defaultOpen={props.defaultOpen}
+				defaultValue={props.defaultValue}
+				autoComplete={props.autoComplete}
+				onOpenChange={props.onOpenChange}
+				onValueChange={props.onValueChange}
+			>
+				<Select.Trigger
+					{...props?.trigger}
+					className={props.triggerClassName}
+				/>
 
-        <Select.Content
-          {...props?.content}
-          position={props?.position ?? ZRUSelectContentPositionE.popper}
-        >
-          {props?.options?.map((option, index) => {
-            return (
-              <Select.Item value={option?.value} key={index}>
-                {option?.label}
-              </Select.Item>
-            );
-          })}
-        </Select.Content>
-      </Select.Root>
+				<Select.Content
+					{...props?.content}
+					position={props?.position ?? ZRUSelectContentPositionE.popper}
+				>
+					{props?.options?.map((option, index) => {
+						return (
+							<Select.Item value={String(option?.value)} key={index}>
+								{option?.label}
+							</Select.Item>
+						);
+					})}
+				</Select.Content>
+			</Select.Root>
 
-      {/* Error */}
-      {props?.isTouched && _isError ? (
-        <ZText
-          as={ZRUTextAsE.span}
-          size="1"
-          color={ZRUColorE.tomato}
-          className="font-medium"
-        >
-          {Array.isArray(props?.errorMessage)
-            ? props?.errorMessage[0]
-            : props?.errorMessage}
-        </ZText>
-      ) : null}
+			{/* Error */}
+			{props?.isTouched && _isError ? (
+				<ZText
+					as={ZRUTextAsE.span}
+					size='1'
+					color={ZRUColorE.tomato}
+					className='font-medium'
+				>
+					{Array.isArray(props?.errorMessage)
+						? props?.errorMessage[0]
+						: props?.errorMessage}
+				</ZText>
+			) : null}
 
-      {/* Info */}
-      {!props?.isTouched && isZNonEmptyString(props?.infoText) ? (
-        <ZText
-          as={ZRUTextAsE.span}
-          size="1"
-          color={ZRUColorE.gold}
-          className="font-medium"
-        >
-          {props?.infoText}
-        </ZText>
-      ) : null}
-    </ZBox>
-  );
+			{/* Info */}
+			{!props?.isTouched && isZNonEmptyString(props?.infoText) ? (
+				<ZText
+					as={ZRUTextAsE.span}
+					size='1'
+					color={ZRUColorE.gold}
+					className='font-medium'
+				>
+					{props?.infoText}
+				</ZText>
+			) : null}
+		</ZBox>
+	);
 };
 
 export default ZRUSelect;
--- src/types/radixUI/index.ts
+++ src/types/radixUI/index.ts
@@ -1,64 +1,64 @@
-import { ZRUOverflowE, ZRUPositionE } from "@enums/radixUI";
-import { Responsive } from "@radix-ui/themes/props";
+import { ZRUOverflowE, ZRUPositionE } from '@enums/radixUI';
+import { Responsive } from '@radix-ui/themes/props';
 
 // #region ===== Interfaces =====
 export interface ZRUStyleI {
-  p?: Responsive<string>;
-  px?: Responsive<string>;
-  py?: Responsive<string>;
-  pt?: Responsive<string>;
-  pr?: Responsive<string>;
-  pb?: Responsive<string>;
-  pl?: Responsive<string>;
-  width?: Responsive<string>;
-  minWidth?: Responsive<string>;
-  maxWidth?: Responsive<string>;
-  height?: Responsive<string>;
-  minHeight?: Responsive<string>;
-  maxHeight?: Responsive<string>;
-  position?: Responsive<ZRUPositionE>;
-  inset?: Responsive<string>;
-  top?: Responsive<string>;
-  right?: Responsive<string>;
-  bottom?: Responsive<string>;
-  left?: Responsive<string>;
-  overflow?: Responsive<ZRUOverflowE>;
-  overflowX?: Responsive<ZRUOverflowE>;
-  overflowY?: Responsive<ZRUOverflowE>;
-  flexBasis?: Responsive<string>;
-  flexShrink?: Responsive<string>;
-  flexGrow?: Responsive<string>;
-  gridColumn?: Responsive<string>;
-  gridColumnStart?: Responsive<string>;
-  gridColumnEnd?: Responsive<string>;
-  gridRow?: Responsive<string>;
-  gridRowStart?: Responsive<string>;
-  gridRowEnd?: Responsive<string>;
+	p?: Responsive<string>;
+	px?: Responsive<string>;
+	py?: Responsive<string>;
+	pt?: Responsive<string>;
+	pr?: Responsive<string>;
+	pb?: Responsive<string>;
+	pl?: Responsive<string>;
+	width?: Responsive<string>;
+	minWidth?: Responsive<string>;
+	maxWidth?: Responsive<string>;
+	height?: Responsive<string>;
+	minHeight?: Responsive<string>;
+	maxHeight?: Responsive<string>;
+	position?: Responsive<ZRUPositionE>;
+	inset?: Responsive<string>;
+	top?: Responsive<string>;
+	right?: Responsive<string>;
+	bottom?: Responsive<string>;
+	left?: Responsive<string>;
+	overflow?: Responsive<ZRUOverflowE>;
+	overflowX?: Responsive<ZRUOverflowE>;
+	overflowY?: Responsive<ZRUOverflowE>;
+	flexBasis?: Responsive<string>;
+	flexShrink?: Responsive<string>;
+	flexGrow?: Responsive<string>;
+	gridColumn?: Responsive<string>;
+	gridColumnStart?: Responsive<string>;
+	gridColumnEnd?: Responsive<string>;
+	gridRow?: Responsive<string>;
+	gridRowStart?: Responsive<string>;
+	gridRowEnd?: Responsive<string>;
 }
 
 export interface ZRUMarginI {
-  m?: Responsive<string>;
-  mx?: Responsive<string>;
-  my?: Responsive<string>;
-  mt?: Responsive<string>;
-  mr?: Responsive<string>;
-  mb?: Responsive<string>;
-  ml?: Responsive<string>;
+	m?: Responsive<string>;
+	mx?: Responsive<string>;
+	my?: Responsive<string>;
+	mt?: Responsive<string>;
+	mr?: Responsive<string>;
+	mb?: Responsive<string>;
+	ml?: Responsive<string>;
 }
 
 export interface ZRUPaddingI {
-  p?: Responsive<string>;
-  px?: Responsive<string>;
-  py?: Responsive<string>;
-  pt?: Responsive<string>;
-  pr?: Responsive<string>;
-  pb?: Responsive<string>;
-  pl?: Responsive<string>;
+	p?: Responsive<string>;
+	px?: Responsive<string>;
+	py?: Responsive<string>;
+	pt?: Responsive<string>;
+	pr?: Responsive<string>;
+	pb?: Responsive<string>;
+	pl?: Responsive<string>;
 }
 
 export interface ZRUSelectValueI {
-  value: string;
-  label: string;
+	value: string | number;
+	label: string | number;
 }
 // #endregion
 
@@ -72,39 +72,39 @@ export type ResponsiveT<T> = T | Record<string, T>;
 /**
  * Type representing the size of radix ui button element.
  */
-export type ZRUSizeT = "1" | "2" | "3" | "4";
+export type ZRUSizeT = '1' | '2' | '3' | '4';
 
 /**
  * Type representing the size of radix ui button element.
  */
-export type ZRUTextSizeT = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+export type ZRUTextSizeT = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
 
 /**
  * Type representing the margin of radix ui elements.
  */
 export type ZRUMarginT =
-  | "0"
-  | "1"
-  | "2"
-  | "3"
-  | "4"
-  | "5"
-  | "6"
-  | "7"
-  | "8"
-  | "9"
-  | "-1"
-  | "-2"
-  | "-3"
-  | "-4"
-  | "-5"
-  | "-6"
-  | "-7"
-  | "-8"
-  | "-9";
+	| '0'
+	| '1'
+	| '2'
+	| '3'
+	| '4'
+	| '5'
+	| '6'
+	| '7'
+	| '8'
+	| '9'
+	| '-1'
+	| '-2'
+	| '-3'
+	| '-4'
+	| '-5'
+	| '-6'
+	| '-7'
+	| '-8'
+	| '-9';
 
 export interface ZRUShowable {
-  show?: boolean;
+	show?: boolean;
 }
 
 // #endregion
